15b39d2d7687d896cd4f293c4d0796059e863c33
marlinbleriaux <80590737+marlinbleriaux@users.noreply.github.com>
2024-06-15 22:53:03 +0100

    add requirement

--- AttendanceSystem.py
+++ AttendanceSystem.py
@@ -269,9 +269,9 @@ if __name__ == '__main__':
         app.run(debug=True, host='192.168.1.100', port=5000)
     else:
         # Sinon, utilisez le port assigné par l'environnement de production (Vercel)
-        port = int(os.environ.get('PORT', 5000))
-        app.run(debug=True, host='0.0.0.0', port=port)
+        # port = int(os.environ.get('PORT', 5000))
+        app.run(debug=True, host='0.0.0.0', port=5000)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>face api!</p>"
